4650eb4c960818962a9615445d3d59b63113aec4
deeeki <deeeki@gmail.com>
2022-03-01 15:38:22 +0900

    Style

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -11,6 +11,7 @@ import Head from 'next/head'
 import { SITE_NAME } from '../../lib/constants'
 import markdownToHtml from '../../lib/markdownToHtml'
 import PostType from '../../types/post'
+import 'highlight.js/styles/github.css';
 
 type Props = {
   post: PostType
